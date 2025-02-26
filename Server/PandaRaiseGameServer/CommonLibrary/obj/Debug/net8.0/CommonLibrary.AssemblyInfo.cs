843eb90c729a6ca73944fb7363b7588a4415af42
kkwkkwk <151744107+kkwkkwk@users.noreply.github.com>
2025-02-26 10:07:26 +0900

    gitignore test

--- Server/PandaRaiseGameServer/CommonLibrary/obj/Debug/net8.0/CommonLibrary.AssemblyInfo.cs
+++ Server/PandaRaiseGameServer/CommonLibrary/obj/Debug/net8.0/CommonLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2843f6b8d5473158143b5f6a98bf8c88202acef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb50f4e1da9923aa645139b9460236b72e97c46")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
