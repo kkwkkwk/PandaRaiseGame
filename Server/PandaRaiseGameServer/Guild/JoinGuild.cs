d54ca2477d36c44c374f3f15eff3b6091f443f89
kkwkkwk <151744107+kkwkkwk@users.noreply.github.com>
2025-04-01 00:50:16 +0900

    길드 가입 신청 시 클라이언트에 성공 여부 전달 안되는 문제 해결 완료

--- Server/PandaRaiseGameServer/Guild/JoinGuild.cs
+++ Server/PandaRaiseGameServer/Guild/JoinGuild.cs
@@ -263,8 +263,8 @@ namespace Guild
     // -----------------------------------------------------------
     public class JoinGuildResponse
     {
-        public bool success;
-        public string? message;
+        public bool success { get; set; }
+        public string? message { get; set; }
     }
 
     // -----------------------------------------------------------
